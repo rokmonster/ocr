5ce7569e72b0a55458447d7d75ad7e06db0ea727
Julius Lisauskas <julius@lisauskas.lt>
2023-02-18 09:20:46 +0200

    remove processing of all files in parallel

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/rokmonster/ocr
 
-go 1.19
+go 1.20
 
 require (
 	github.com/Masterminds/sprig/v3 v3.2.3
@@ -29,8 +29,8 @@ require (
 	github.com/Masterminds/semver/v3 v3.2.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
@@ -52,7 +52,7 @@ require (
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
-	github.com/ugorji/go/codec v1.2.8 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- internal/pkg/rokocr/tesseractutils/recognition.go
+++ internal/pkg/rokocr/tesseractutils/recognition.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sync"
 	"time"
 
 	"github.com/rokmonster/ocr/internal/pkg/utils/fileutils"
@@ -14,28 +13,35 @@ import (
 	schema "github.com/rokmonster/ocr/internal/pkg/ocrschema"
 )
 
-func RunRecognitionChan(mediaDir, tessData string, template schema.OCRTemplate, force bool) <-chan schema.OCRResult {
-	out := make(chan schema.OCRResult)
+// func processSingleFile(index, total int, f string) *schema.OCRResult {
+// 	start := time.Now()
+// 	result, err := ParseSingleFile(f, tessData, template, force)
+// 	if err != nil {
+// 		logrus.Printf("[%04d/%04d] %v - %v", index, total, filepath.Base(f), err)
+// 		return
+// 	}
+// 	logrus.Printf("[%04d/%04d] %v Took: %v ms", index, total, filepath.Base(f), time.Since(start).Milliseconds())
+// 	return result
+// }
 
+func RunRecognitionChan(mediaDir, tessData string, template schema.OCRTemplate, force bool) <-chan schema.OCRResult {
+
+	out := make(chan schema.OCRResult)
 	go func() {
-		var wg sync.WaitGroup
 		dir, _ := filepath.Abs(mediaDir)
 		files := fileutils.GetFilesInDirectory(dir)
-		for i, f := range files {
-			wg.Add(1)
-			go func(index, total int, f string) {
-				defer wg.Done()
-				start := time.Now()
-				result, err := ParseSingleFile(f, tessData, template, force)
-				if err != nil {
-					logrus.Printf("[%04d/%04d] %v - %v", index, total, filepath.Base(f), err)
-					return
-				}
-				logrus.Printf("[%04d/%04d] %v Took: %v ms", index, total, filepath.Base(f), time.Since(start).Milliseconds())
-				out <- *result
-			}(i+1, len(files), f)
+		total := len(files)
+
+		for index, f := range files {
+			start := time.Now()
+			result, err := ParseSingleFile(f, tessData, template, force)
+			if err != nil {
+				logrus.Printf("[%04d/%04d] %v - %v", index, total, filepath.Base(f), err)
+				continue
+			}
+			logrus.Printf("[%04d/%04d] %v Took: %v ms", index, total, filepath.Base(f), time.Since(start).Milliseconds())
+			out <- *result
 		}
-		wg.Wait()
 		close(out)
 	}()
 
